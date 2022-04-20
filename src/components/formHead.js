aeac95010037024517eb2c4be86e2b704d60bb83
goldphilith <aniokesam@gmail.com>
2022-04-20 03:51:46 -0700

    test

--- src/components/formHead.js
+++ src/components/formHead.js
@@ -6,6 +6,7 @@ import GeneralInfo from './generalInfo';
     <div className='fromHead'>
         <h1>CV Document Builder</h1>
         <p>Welcome to the beginning of landing your dream job</p>
+        <p>Wih our help you will make the perfect CV</p>
         <GeneralInfo />
     </div>)
   
--- src/components/generalInfo.js
+++ src/components/generalInfo.js
@@ -11,11 +11,11 @@ import ProfilePic from './profilePic_and_CV';
         <ProfilePic />
        <div className='form-head'> 
            <label for = "name">Full Name</label>
-        <input type="text" id='name'></input>
+        <input type="text" id='name' value="Chimdi Anioke"></input>
         <label for = "email">Email</label>
-        <input type="email" id='email'></input>
+        <input type="email" id='email'placeholder='your-name@gmail.com'></input>
         <label for = "number">Phone No</label>
-        <input type="number" id='number'></input></div>
+        <input type="number" id='number' placeholder='08012345678'></input></div>
         <Education />
         
         </form>
